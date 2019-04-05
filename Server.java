f283b919fb7d8a62c92b5e04c147b5bf560d4311
Daniel Escamilla <danix100@hotmail.com>
2019-04-05 17:36:16 -0600

    Cambios a servidor para ping constante independiente de clientes

--- Server.java
+++ Server.java
@@ -3,21 +3,73 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.io.*;
 
 public class Server {
+
+    private static LinkedBlockingDeque colaMensajes;
+    private static ServerSocket serverSocket;
+    private static final int PORT = 1234;
+    private static String Group = "224.0.0.1";
+    private static byte TTL = 3;
     public static void main(String[] args) {
         System.out.println("Inicio el server...");
-        LinkedBlockingDeque colaMensajes = new LinkedBlockingDeque<String>();
+        colaMensajes = new LinkedBlockingDeque<String>();
+
         try {
-            ServerSocket serverSocket = new ServerSocket(5000);
-            while (true) {
-                System.out.println("Listening for a client connection");
-                Socket socket = serverSocket.accept();
-                System.out.println("Connected to a client");
-                new Thread(new ServerThread(socket,colaMensajes)).start();
-            }
-        } catch (SocketException ex) {
-            System.out.println("Cliente desconectado I guess");
-        } catch (IOException ex) {
+            serverSocket = new ServerSocket(5000);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        Thread threadPublica = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("Iniciando server publicacion");
+                try {
+                    String msg;
+                    MulticastSocket ms = new MulticastSocket();
+                    byte buf[] = new byte[1024];
+                    DatagramPacket pack;
+                    while (true) {
+                        msg = "Server heartbeat";
+                        if (!colaMensajes.isEmpty()) {
+                            if (colaMensajes.peekFirst().toString().equals("null")) {
+                                //Mensaje null = ultimo mensaje de cliente desconectado
+                                colaMensajes.removeFirst();
+                            }
+                            if (!colaMensajes.isEmpty()) {
+                                msg = (String) colaMensajes.pollFirst();
+                            }
+                        }
+                        pack = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName(Group), PORT);
+                        ms.send(pack, TTL);
+                        sleep(10000);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+
+        Thread threadEscucha = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        Socket socket = serverSocket.accept();
+                        System.out.println("Cliente conectado");
+                        new Thread(new ServerThread(socket,colaMensajes)).start();
+                    }
+                } catch (SocketException ex) {
+                    System.out.println("Excepcion socket exception");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+
+        threadPublica.start();
+        threadEscucha.start();
+        System.out.println("Escuchando conexiones de clientes...");
     }
 }
+
+
--- ServerThread.java
+++ ServerThread.java
@@ -7,12 +7,10 @@ import java.net.Socket;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
+import java.net.SocketException;
 
 public class ServerThread extends Thread {
     private Socket clientSocket;
-    private final int PORT = 1234;
-    private String Group = "224.0.0.1";
-    private byte TTL = 3;
     private LinkedBlockingDeque colaMensajes;
     private BufferedReader inClient;
 
@@ -26,50 +24,18 @@ public class ServerThread extends Thread {
 
     @Override
     public void run() {
+        System.out.println("Iniciando thread escucha");
         try {
-            Thread threadEscucha = new Thread() {
-                @Override
-                public void run() {
-                    System.out.println("Iniciando thread escucha");
-                    try {
-                        inClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        while (true) {
-                            String msg = inClient.readLine();
-                            System.out.println("Mensaje recibido: " + msg);
-                            colaMensajes.addLast(msg);
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
+            inClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while (true) {
+                String msg = inClient.readLine();
+                System.out.println("Mensaje recibido: " + msg);
+                if (msg!=null) {
+                    colaMensajes.addLast(msg);
                 }
-            };
-
-            Thread threadPublica = new Thread() {
-                @Override
-                public void run() {
-                    System.out.println("Iniciando thread publicacion");
-                    try {
-                        String msg;
-                        MulticastSocket ms = new MulticastSocket();
-                        byte buf[] = new byte[1024];
-                        DatagramPacket pack;
-                        while (true) {
-                            msg = "Server heartbeat";
-                            if (!colaMensajes.isEmpty()) {
-                                msg = (String) colaMensajes.pollFirst();
-                            }
-                            pack = new DatagramPacket(msg.getBytes(), msg.length(), InetAddress.getByName(Group), PORT);
-                            ms.send(pack, TTL);
-                            sleep(10000);
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            };
-
-            threadEscucha.start();
-            threadPublica.start();
+            }
+        } catch (SocketException ex) {
+            System.out.println("Cliente desconectado");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
